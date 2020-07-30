8243c3edeb3ccd89e27a95ab66f44a60ee37f835
Linda Lam <llam7300@gmail.com>
2020-07-30 16:04:50 -0400

    Create Day2_Conditional_Statements_Switch.js

---
+++ 10 Days of JS/Day2_Conditional_Statements_Switch.js
@@ -0,0 +1,32 @@
+
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch (true) {
+        case 'aeiou'.includes(s[0]):
+            letter = "A";
+            break;
+
+        case 'bcdfg'.includes(s[0]):
+            letter = "B";
+            break;
+
+        case 'hjklm'.includes(s[0]):
+            letter = "C";
+            break;
+
+        case 'npqrstvwyz'.includes(s[0]):
+            letter = "D";
+            break;
+    }   
+    return letter;
+}
+
+/**
+Input (stdin)
+adfgt
+
+Expected Output
+A
+**/

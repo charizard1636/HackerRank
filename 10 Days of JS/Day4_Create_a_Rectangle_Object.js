f05a06e1ec6a0b05a463b1d776cdc6aa1ace4da5
Linda Lam <llam7300@gmail.com>
2020-07-30 16:26:55 -0400

    Create Day4_Create_a_Rectangle_Object.js

---
+++ 10 Days of JS/Day4_Create_a_Rectangle_Object.js
@@ -0,0 +1,23 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a * b
+    };
+}
+
+/**
+Input (stdin)
+4
+5
+
+Expected Output
+4
+5
+18
+20
+**/

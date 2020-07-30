e4d578bc32a37e2d73cc36cb5f1040b3ca768c93
Linda Lam <llam7300@gmail.com>
2020-07-30 16:53:46 -0400

    Create Day7_Regular_Expressions_II.js

---
+++ 10 Days of JS/Day7_Regular_Expressions_II.js
@@ -0,0 +1,24 @@
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    
+    let re = /^(Mr|Mrs|Ms|Dr|Er)(\.)([a-zA-Z]+$)/
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+/**
+Input (stdin)
+Mr.X
+
+Expected Output
+true
+**/

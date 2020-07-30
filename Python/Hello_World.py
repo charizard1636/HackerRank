3c2a12db9d172d270e1d13be16a3e64e77c9453e
Linda Lam <llam7300@gmail.com>
2020-07-30 17:56:39 -0400

    Create Hello_World.py

---
+++ Python/Hello_World.py
@@ -0,0 +1,18 @@
+"""
+Here is a sample line of code that can be executed in Python:
+
+print("Hello, World!")
+You can just as easily store a string as a variable and then print it to stdout:
+
+my_string = "Hello, World!"
+print(my_string)
+"""
+
+if __name__ == '__main__':
+    print "Hello, World!"
+    
+    
+"""
+Expected Output
+Hello, World!
+"""

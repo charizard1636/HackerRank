2e75534f750e6913146d14b0c88a3b3db19359f7
Linda Lam <llam7300@gmail.com>
2020-08-06 11:34:36 -0400

    Create Repeated_String.js

---
+++ Interview Preparation Kit/Warm-up Challenges/Repeated_String.js
@@ -0,0 +1,74 @@
+/**
+Lilah has a string, s, of lowercase English letters that she repeated infinitely many times.
+
+Given an integer, n, find and print the number of letter a's in the first  letters of Lilah's infinite string.
+
+For example, if the string s= 'abcac' and n = 10, the substring we consider is abcacabcac, the first 10 characters of her infinite string. There are 4 occurrences of a in the substring.
+
+Function Description
+
+Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length  in the infinitely repeating string.
+
+repeatedString has the following parameter(s):
+
+s: a string to repeat
+n: the number of characters to consider
+Input Format
+
+The first line contains a single string, s.
+The second line contains an integer, n.
+**/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the repeatedString function below.
+function repeatedString(s, n) {
+    let count = (s.match(/a/g) || []).length * Math.floor(n/s.length);
+    let remainder = n % s.length;
+
+    for(var i = 0; i < remainder;  i++) {
+        if(s.charAt(i) === "a") {
+        count++;
+        }
+    }
+    return count;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine(), 10);
+
+    let result = repeatedString(s, n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
